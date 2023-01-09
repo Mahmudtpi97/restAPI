36df2f2487c7574a457f2e51eeb4c7ee2143b962
MD Mahmudul Hasan <www.mahmudulmh@gmail.com>
2023-01-09 23:55:12 +0600

    Use Laravel Model to Caching data from database

--- app/Http/Controllers/EmployeeController3.php
+++ app/Http/Controllers/EmployeeController3.php
@@ -6,35 +6,54 @@ namespace App\Http\Controllers;
 
 use App\Models\Employee;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class EmployeeController3 extends Controller
 {
-
+   // allData
     public function allData(){
         $result = Employee::all();
-
         return  response()->json($result);
     }
+    // insertData
+    public function dataInsert(Request $request){
+        $result = $request->all();
+        if (Employee::create($result)) {
+            return "Insert Success";
+        }
+        else{
+            return "Insert Failed";
+        }
 
-    // Aggregates Using Model
-    public function count(){
-        $result = Employee::count('phone');
-        return $result;
     }
-    public function sum(){
-        $result = Employee::sum('status');
-        return $result;
+    // updateData only name and cat
+    public function dataUpdate(Request $request){
+        $id       = $request->input('id');
+        $name       = $request->input('name');
+        $cat       = $request->input('cat');
+
+        $formData = DB::table('employee')->where('id',$id)->update(['name'=>$name,'cat'=>$cat]);
+
+        if ($formData == true) {
+            return "Update Success";
+        }
+        else{
+            return "Update Failed";
+        }
+
     }
-    public function max(){
-        $result = Employee::max('phone');
-        return $result;
+    // deleteData
+    public function dataDelete(Request $request){
+        $id       = $request->input('id');
+        $result = Employee::findOrFail($id);
+
+        if ($result->delete()) {
+            return "Delete Success";
+        }
+        else{
+            return "Delete Failed";
+        }
+
     }
-    public function min(){
-        $result = Employee::min('phone');
-        return $result;
-    }
-    public function avg(){
-        $result = Employee::avg('phone');
-        return $result;
-    }
+
 }
